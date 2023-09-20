0f103820b71c4d454b9835a72a666821eb21dbd4
yashsahni1 <145418627+yashsahni1@users.noreply.github.com>
2023-09-20 13:05:43 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        int n = 3; // Change this value to control the number of rows
+int count=1;
+        for (int i = 0; i < n; i++) {
+            // Print spaces before the numbers
+            for (int j = 0; j < n - i-1; j++) {
+                System.out.print(" ");
+            }
+
+            // Print the numbers
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print(count);
+                count++;
+                
+            }
+            count=1;
+            System.out.println(); // Move to the next line
+        }
+    
+	}
+}
