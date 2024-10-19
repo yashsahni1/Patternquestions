8b37a8517885bb304388b40a87cc6e467547731d
yash sahni <sahniyash96@gmail.com>
2024-10-19 13:58:04 +0530

    uuu

--- pattern7.java
+++ pattern7.java
@@ -14,12 +14,12 @@ public class Main
    for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
-                    System.out.print("* ");
+                    System.out.print("* ");//////
                 } else {
                     System.out.print("  "); // Two spaces for each space
                 }
             }
-            System.out.println();
+            System.out.println();//////
         }
 	}
 }
