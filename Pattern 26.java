35a86986fe61f583ca5c17504717bd95474f3089
yash sahni <sahniyash96@gmail.com>
2024-10-19 13:55:09 +0530

    yash

--- Pattern 26.java
+++ Pattern 26.java
@@ -11,7 +11,7 @@ public class Main
 	public static void main(String[] args) {
         int n = 3; // Change this value to control the number of rows
 int count=1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {///////////
             // Print spaces before the numbers
             for (int j = 0; j < n - i-1; j++) {
                 System.out.print(" ");/////
--- pattern1.java
+++ pattern1.java
@@ -10,11 +10,10 @@ public class Main{
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n=scn.nextInt();
-		for(int i=0;i<n;i++){
+		for(int i=0;i<n;i++){///////
 		    for(int j=0;j<n;j++){
-		        System.out.print("* ");
+		        System.out.print("* ");//////
 		    }
 		    System.out.println();
 		}
 	}
-}
